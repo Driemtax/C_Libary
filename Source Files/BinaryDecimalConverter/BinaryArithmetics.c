caeba34c5bd953488e179d8e53483776b773380b
Lars Beer <lars.beer@stud.uni-frankfurt.de>
2024-03-28 21:50:01 +0100

    Addition seems to work

--- Source Files/BinaryDecimalConverter/BinaryArithmetics.c
+++ Source Files/BinaryDecimalConverter/BinaryArithmetics.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 char* BinaryAddition(char *summand1, char *summand2, int size){
     int carryBit = 0;
 
-    char *result = (char)malloc(size * sizeof(char));
+    char *result = (char*)malloc(size * sizeof(char));
 
     //change with  utility function when header files are working
     for (int i = 0; i < size; i++)
@@ -11,7 +12,7 @@ char* BinaryAddition(char *summand1, char *summand2, int size){
         result[i] = '0';
     }
 
-    for (int i = size; i > 0; i--)
+    for (int i = size - 1; i >= 0; i--)
     {
         if (summand1[i] == '0' && summand2[i] == '0') //case both bits = 0
         {
@@ -22,25 +23,65 @@ char* BinaryAddition(char *summand1, char *summand2, int size){
             }
             else{
                 result[i] = '0';
+                carryBit = 0;
             }
         }
         else if(summand1[i] == '1' && summand2[i] == '0') // case summand 1 = 1 and summand 2 = 0
         {
-            result[i] = '1';
+            if (carryBit)
+            {
+                result[i] = '0';
+                carryBit = 1;
+            }
+            else{
+                result[i] = '1';
+                carryBit = 0;
+            }
         }
-        else if(summand1[i] == '0' && summand2[i] == '1')
+        else if(summand1[i] == '0' && summand2[i] == '1') // case summand 1 = 0 and summand 2 = 1
         {
-            result[i] = '1';
+            if (carryBit)
+            {
+                result[i] = '0';
+                carryBit = 1;
+            }
+            else
+            {
+                result[i] = '1';
+                carryBit = 0;
+            }
         }
-        else if (summand1 == '1' && summand2 == '1')
+        else if (summand1[i] == '1' && summand2[i] == '1') // case summand 1 = 1 and summand 2 = 1
         {
-            carryBit = 1;
-            result[i] = '0';
+            if (carryBit)
+            {
+                result[i] = '1';
+                carryBit = 1;
+            }
+            else
+            {
+                result[i] = '0';
+                carryBit = 1;
+            }
         }   
     }
+
+    return result;
 }
 
 int main(){
-    char testNumber1[] = "11000011";
-    char testNumber2[] = "00000011";
+    char testNumber1[] = "00000001"; // 100
+    char testNumber2[] = "00000011"; //  95
+                        // exp Result:  11000011
+    char *resultEnd = (char*)malloc(8 * sizeof(char));
+    resultEnd = BinaryAddition(testNumber1, testNumber2, 8);
+
+    printf("Result: ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%c", resultEnd[i]);
+    }
+
+    getchar();
+    
 }
