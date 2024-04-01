a00472a8c6cae88805b181a5470d9477a89af0d0
Lars <beer.lars@yahoo.de>
2024-04-01 20:02:25 +0200

    Selection Sort

---
+++ Source Files/selection_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <helper.h>
+
+#define SIZE 100
+
+void selection_sort(int *arr, int size){
+    for (int i = 0; i < size; i++)
+    {
+        int min_index;
+        for (int j = i+1; j < size; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = arr[j];
+            }
+        }
+        swap_numbers(&arr[i], &arr[min_index]);
+    }
+    
+}
+
+int main(){
+    int *arr = array_malloc(SIZE);
+    randomize_array(arr, SIZE);
+
+    selection_sort(arr, SIZE);
+
+    //print Output
+    printf("Sorted Array: ");
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+}
