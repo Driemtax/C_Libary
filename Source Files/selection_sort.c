87317f779c98504f8af569a94d6f087111649c29
Lars <driemtax@gmail.com>
2024-04-10 19:26:46 +0200

    finished Implementation of selection sort

--- Source Files/selection_sort.c
+++ Source Files/selection_sort.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <helper.h>
+#include "../Header Files/helper.h"
 
 #define SIZE 100
 
 void selection_sort(int *arr, int size){
     for (int i = 0; i < size; i++)
     {
-        int min_index;
+        int min_index = i;
         for (int j = i+1; j < size; j++)
         {
             if (arr[j] < arr[min_index])
             {
-                min_index = arr[j];
+                min_index = j;
             }
         }
         swap_numbers(&arr[i], &arr[min_index]);
