9038416148465d90b816f72984da688c2257acc4
Lars Beer <driemtax@gmail.com>
2024-05-18 18:29:48 +0200

    started merge_sort

--- Header Files/helper.h
+++ Header Files/helper.h
@@ -5,5 +5,6 @@ void randomize_array(int *arr, int size);
 int* array_malloc(int size);
 char* initialize_char_array(char *arr, int size);
 int* initialize_int_array(int *arr, int size);
+void swap_numbers(int *a, int *b);
 
 #endif
--- Source Files/sort.c
+++ Source Files/sort.c
@@ -63,6 +63,84 @@ void test_insertion_sort(){
     }
 }
 
+int* merge(int* left, int* right, int size){
+    int* result = array_malloc(size);
+    int left_idx = 0; int right_idx = 0;
+
+    int counter = 0;
+    while (left_idx < size && right_idx < size)
+    {
+        if (left[left_idx] <= right[right_idx])
+        {
+            result[counter] = left[left_idx];
+            counter++;
+            left_idx++;
+        }
+        else
+        {
+            result[counter] = right[right_idx];
+            counter++;
+            right_idx++;
+        }
+    }
+
+    while (left_idx < size)
+    {
+        result[counter] = left[left_idx];
+        counter++;
+        left_idx++;        
+    }
+    while (right_idx < size)
+    {
+        result[counter] = right[right_idx];
+        counter++;
+        right_idx++;
+    }
+
+    return result;
+}
+
+int* merge_sort(int *arr, int size){
+    if (size <= 1)
+    {
+        return arr;
+    }
+
+    int mid = size / 2;
+
+    int* left = array_malloc(mid);
+    for (int i = 0; i < mid; i++)
+    {
+        left[i] = arr[i];
+    }
+
+    int* right = array_malloc(mid);
+    for (int i = mid; i < size; i++)
+    {
+        right[i] = arr[i];
+    }
+
+    left = merge_sort(left, mid);
+    right = merge_sort(right, mid);
+
+
+    return merge(left, right, mid);
+}
+
+void test_merge_sort(){
+    int *arr = array_malloc(SIZE);
+    randomize_array(arr, SIZE);
+
+    merge_sort(arr, SIZE);
+
+    //print Output
+    printf("Sorted Array: ");
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+}
+
 int main(){
-    test_insertion_sort();
+    test_merge_sort();
 }
